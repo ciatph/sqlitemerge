d33cc56b47101bc5f46ad7b5f498e3edd12bc139
= <=>
2018-04-11 16:46:35 +0700

    Added documentation header.
    Removed schema.sql.

--- init.bat
+++ init.bat
@@ -1,3 +1,10 @@
+::----------------------------------------------------------
+:: Create a master sqlite database file 
+:: - following the AppDataCollect schema,
+:: - modified to have an autoincrement _id index
+:: angel;20180411
+::----------------------------------------------------------
+
 @echo off
 
 rem clear the input variable
@@ -11,7 +18,7 @@ GOTO Processing
 
 :Processing
  echo Processing %dbfile%...
- sqlite3 %dbfile% > settings/schema.sql .schema
+ :: sqlite3 %dbfile% > settings/schema.sql .schema
  
  echo Creating new master database "maindb.db"...
  sqlite3 maindb.db < settings/schema_sqlite.sql
