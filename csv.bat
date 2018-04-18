e00684ade1d60618fcef23545d86577437923d56
ciatph <ciat.ph@gmail.com>
2018-04-18 17:07:29 +0700

    Committed scripts for exporting farmland data to CSV file.

---
+++ csv.bat
@@ -0,0 +1,6 @@
+::----------------------------------------------------------
+:: Export selected (uncleaned) farmland data into a CSV file
+:: angel;20180417
+::----------------------------------------------------------
+
+sqlite3 maindb.db < settings/commands_csv.txt
