d15c2b99813211f7ac445921fd77fcd4aeb9d6c0
ciatph <ciat.ph@gmail.com>
2018-04-18 17:05:08 +0700

    Deleted all *.db files. Restored external /databases repository to local state.

--- clean.bat
+++ clean.bat
@@ -8,17 +8,27 @@
 GOTO Main
 
 :Main
-  rem clear the input variable
+  rem Clear the input variable
   set "resetAll="
 
   echo Are you sure you want to reset everything?
   set /p resetAll="";
 
   if "%resetAll%" == "" GOTO End
-  
+
   del *.sql
-  del maindb.db
+  del *.csv
+  del *.db
+
+  :: Remove references to /databases
+  git remote rm databases 
+  git rm -r databases
+  rmdir /S /Q databases
+
+  :: Restore /databases to original state
+  git reset HEAD databases
+  git checkout databases
 EXIT /B 0
  
 :End
- pause;
+ EXIT /B 0
